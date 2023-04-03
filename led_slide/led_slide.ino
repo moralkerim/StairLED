ff94197e17ed528559db1300b8b87e63cab6d0d3
Kerim <moralkerim@gmail.com>
2023-04-03 16:40:43 +0300

    Slide on/off with ultrasonic.

--- led_slide/led_slide.ino
+++ led_slide/led_slide.ino
@@ -1,11 +1,22 @@
 #include <FastLED.h> //Kütüphane import 
+#include "NewPing.h"
 
-#define DEBUG
+//#define DEBUG
 
 #define NUM_LEDS 4 //Led Sayısı
 #define DATA_PIN 7 //Led Din giriş pini
+#define TRIGGER_PIN 9
+#define ECHO_PIN 10
+
+#define MAX_DISTANCE 400
+#define TOGGLE_RATE 2000
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+float duration, distance;
+
 CRGB leds[NUM_LEDS]; //Led Dizisi oluşturduk
-int8_t last_led = 0;
+int8_t last_led = -1;
+bool out_of_range;
+unsigned long toggle_time;
 
 typedef enum LED_STATE
 {
@@ -22,25 +33,47 @@ void setup() {
   TurnOffLeds();
 }
 void loop() {
-  if (led_state == turn_on) {
-    LedSlide();
+  MeasureDistance();
+
+  if (!out_of_range) {
+    if (distance < 100 && millis() - toggle_time > TOGGLE_RATE) {
+      toggle_time = millis();
+      ToggleLED();
+    }
   }
 
-  else if (led_state == turn_off) {
-    LedSlideOff();
-  }
-  CheckOnOff();
+  switch (led_state) {
+    case turn_on:
+      LedSlide();
+      break;
 
+    case turn_off:
+      LedSlideOff();
+      break;
+  }
+
+#ifdef DEBUG
+  Serial.print("last_led = ");  Serial.println(last_led);
+  Serial.print("led_state = "); Serial.println(led_state);
+  Serial.print("distance = ");  Serial.println(distance);
+  Serial.println("--------------");
+#endif
 
   delay(100);
 }
 
 void LedSlide() {
+  if (last_led < NUM_LEDS - 1) {
+    last_led++;
+  }
   leds[last_led].setRGB(255, 255, 255); //sonraki 5 ledi
   FastLED.show();
 }
 
 void LedSlideOff() {
+  if (last_led > 0) {
+    last_led--;
+  }
   leds[last_led].setRGB(0, 0, 0); //sonraki 5 ledi
   FastLED.show();
 }
@@ -52,6 +85,7 @@ void CheckOnOff() {
       if (last_led == NUM_LEDS) {
         led_state = turn_off;
         last_led--;
+        delay(1000);
       }
       break;
     case turn_off:
@@ -59,15 +93,44 @@ void CheckOnOff() {
       if (last_led == -1) {
         led_state = turn_on;
         last_led++;
+        delay(1000);
       }
       break;
   }
-
 #ifdef DEBUG
   Serial.print("last_led = "); Serial.println(last_led);
   Serial.print("led_state = "); Serial.println(led_state);
   Serial.println("--------------");
 #endif
+}
+
+void ToggleLED() {
+  switch (led_state) {
+    case turn_on:
+      last_led = NUM_LEDS;
+      led_state = turn_off;
+      break;
+
+    case turn_off:
+
+      last_led = -1;
+      led_state = turn_on;
+      break;
+  }
+}
+
+
+void MeasureDistance() {
+  distance = sonar.ping_cm();
+  //Serial.println(distance);
+  if (distance >= 400 || distance <= 2)
+  {
+    out_of_range = true;
+  }
+
+  else {
+    out_of_range = false;
+  }
 }
 
 void TurnOffLeds() {
